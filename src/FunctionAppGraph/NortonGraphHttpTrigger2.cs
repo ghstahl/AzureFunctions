c3776bd68fdd923c9cd1aa82785c9417a9ea8aa9
ghstahl@gmail.com <ghstahl@gmail.com>
2018-03-08 10:36:42 -0800

    using the following code to test https://github.com/Azure-Samples/azure-cosmos-db-graph-dotnet-getting-started/blob/35c50b2062c26a1e9b9eb23af5d4c26c5c5856ac/GraphGetStarted/Program.cs

--- src/FunctionAppGraph/NortonGraphHttpTrigger2.cs
+++ src/FunctionAppGraph/NortonGraphHttpTrigger2.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.Graphs;
 using Microsoft.Azure.Graphs.Elements;
 using Microsoft.Azure.WebJobs;
@@ -17,6 +19,28 @@ namespace FunctionAppGraph
 {
     public class NortonGraphHttpTrigger2
     {
+        static Dictionary<string, string> GremlinQueries = new Dictionary<string, string>
+        {
+            { "Cleanup",        "g.V().drop()" },
+            { "AddVertex 1",    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)" },
+            { "AddVertex 2",    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)" },
+            { "AddVertex 3",    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')" },
+            { "AddVertex 4",    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')" },
+            { "AddEdge 1",      "g.V('thomas').addE('knows').to(g.V('mary'))" },
+            { "AddEdge 2",      "g.V('thomas').addE('knows').to(g.V('ben'))" },
+            { "AddEdge 3",      "g.V('ben').addE('knows').to(g.V('robin'))" },
+            { "UpdateVertex",   "g.V('thomas').property('age', 44)" },
+            { "CountVertices",  "g.V().count()" },
+            { "Filter Range",   "g.V().hasLabel('person').has('age', gt(40))" },
+            { "Project",        "g.V().hasLabel('person').values('firstName')" },
+            { "Sort",           "g.V().hasLabel('person').order().by('firstName', decr)" },
+            { "Traverse",       "g.V('thomas').out('knows').hasLabel('person')" },
+            { "Traverse 2x",    "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person')" },
+            { "Loop",           "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()" },
+            { "DropEdge",       "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()" },
+            { "CountEdges",     "g.E().count()" },
+            { "DropVertex",     "g.V('thomas').drop()" },
+        };
         [FunctionName("NortonGraphHttpTrigger2")]
         public static async Task RunAsync(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]
@@ -27,15 +51,21 @@ namespace FunctionAppGraph
 
             string documentsEndpoint = ConfigurationManager.AppSettings["documentsendpoint"];
             string graphDbKey = ConfigurationManager.AppSettings["graphdbkey"];
-            string database = ConfigurationManager.AppSettings["database"];
+            string databaseName = ConfigurationManager.AppSettings["database"];
             string documentCollection = ConfigurationManager.AppSettings["collection"];
 
             log.Info($"   documentsEndpoint:{documentsEndpoint}");
             log.Info($"   graphDbKey:{graphDbKey}");
-            log.Info($"   database:{database}");
+            log.Info($"   databaseName:{databaseName}");
             log.Info($"   documentCollection:{documentCollection}");
 
             var client = new DocumentClient(new Uri(documentsEndpoint), graphDbKey);
+            Database database = await client.CreateDatabaseIfNotExistsAsync(new Database { Id = databaseName });
+            DocumentCollection graph = await client.CreateDocumentCollectionIfNotExistsAsync(
+                UriFactory.CreateDatabaseUri(databaseName),
+                new DocumentCollection { Id = documentCollection },
+                new RequestOptions { OfferThroughput = 1000 });
+
             var data = new Dictionary<string, dynamic>()
             {
                 {"id", Guid.NewGuid()},
@@ -54,19 +84,47 @@ namespace FunctionAppGraph
                 male = true,
                 age = 32
             };
-            var query = QueryAddVertex("user", data);
-            log.Info($"   query:{query}");
+            var query1 = QueryAddVertex("user", data);
             var query2 = QueryAddVertex("user", data2);
-            log.Info($"   query2:{query2}");
-            var uri = UriFactory.CreateDatabaseUri(database);
-           // log.Info($" databaseUri: {uri.AbsolutePath}");
 
-            var collection = await client.CreateDocumentCollectionIfNotExistsAsync(uri, new DocumentCollection { Id = documentCollection });
 
-            var resp = await client.CreateGremlinQuery<Vertex>(collection, query).ExecuteNextAsync();
+            foreach (KeyValuePair<string, string> gremlinQuery in GremlinQueries)
+            {
+                log.Info($"Running {gremlinQuery.Key}: {gremlinQuery.Value}");
+
+                // The CreateGremlinQuery method extensions allow you to execute Gremlin queries and iterate
+                // results asychronously
+                IDocumentQuery<dynamic> query = client.CreateGremlinQuery<dynamic>(graph, gremlinQuery.Value);
+                while (query.HasMoreResults)
+                {
+                    foreach (dynamic result in await query.ExecuteNextAsync())
+                    {
+                        log.Info($"\t {JsonConvert.SerializeObject(result)}");
+                    }
+                }
+            }
+
+            // Data is returned in GraphSON format, which be deserialized into a strongly-typed vertex, edge or property class
+            // The following snippet shows how to do this
+            string gremlin = GremlinQueries["AddVertex 1"];
+            log.Info($"Running Add Vertex with deserialization: {gremlin}");
+
+            IDocumentQuery<Vertex> insertVertex = client.CreateGremlinQuery<Vertex>(graph, GremlinQueries["AddVertex 1"]);
+            while (insertVertex.HasMoreResults)
+            {
+                foreach (Vertex vertex in await insertVertex.ExecuteNextAsync<Vertex>())
+                {
+                    // Since Gremlin is designed for multi-valued properties, the format returns an array. Here we just read
+                    // the first value
+                    string name = (string)vertex.GetVertexProperties("firstName").First().Value;
+                    log.Info($"\t Id:{vertex.Id}, Name: {name}");
+                }
+            }
+
+            var resp = await client.CreateGremlinQuery<Vertex>(graph, query1).ExecuteNextAsync();
             log.Info($"C# function processed: {resp.ActivityId}");
 
-            var resp2 = await client.CreateGremlinQuery<Vertex>(collection, query2).ExecuteNextAsync();
+            var resp2 = await client.CreateGremlinQuery<Vertex>(graph, query2).ExecuteNextAsync();
             log.Info($"C# function processed: {resp2.ActivityId}");
             log.Info($"C# function processed: NortonGraphHttpTrigger2");
         }
